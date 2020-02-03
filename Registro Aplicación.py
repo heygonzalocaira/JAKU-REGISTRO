6b8a17f799ca7c3e036f9a96797f5c349e39b3ad
Gonzalo Alejandro Caira Aguilar <gc98wootz@gmail.com>
2020-02-03 09:43:59 -0500

    first commit

---
+++ Registro Aplicación.py
@@ -0,0 +1,190 @@
+
+import pygame 
+import csv
+import os
+from teams import *
+import pandas as pd
+from time import gmtime, strftime
+from openpyxl import Workbook
+
+date = strftime("%Y-%m-%d", gmtime())
+file_csv = "Registro CSV " 
+file_csv = file_csv + date +".csv"
+#print(list_teams)
+def change_assistance(var,list_teams):
+    str(var)
+    for one_list in list_teams:
+        ##if one_list.assistance == 'NO':
+        if one_list.find_DNI(var):
+            one_list.change_assistance()
+            one_list.change_date()
+            return True
+    return False
+def change_endTime(var,list_teams):
+    var = str(var)
+    for one_list in list_teams:
+            if one_list.find_DNI(var):
+                one_list.change_endT()
+                return True
+    return False
+def write_assitance(list_teams,file_csv):
+    
+    with open(file_csv, 'w', newline='') as file:
+                    fieldnames = ['Nombre del emprendimiento', 'Asistencia','Dia','Hora Entrada',"Hora Salida"]
+                    writer = csv.DictWriter(file, fieldnames=fieldnames)
+                    writer.writeheader()
+                    for one_list in list_teams:
+                        writer.writerow({'Nombre del emprendimiento': one_list.name, 
+                                            'Asistencia': one_list.assistance,
+                                            'Dia':one_list.date,
+                                            'Hora Entrada':one_list.startTime,
+                                            'Hora Salida':one_list.endTime})
+    
+
+
+pygame.init()
+screen = pygame.display.set_mode((900, 600))
+COLOR_INACTIVE = pygame.Color('black')
+COLOR_ACTIVE = pygame.Color('blue')
+font_path = "Fipps-Regular.otf"
+FONT = pygame.font.Font(font_path, 32)
+
+
+class InputBox: 
+    def __init__(self, x, y, w, h, text=''):
+        self.rect = pygame.Rect(x, y, w, h)
+        self.color = COLOR_INACTIVE
+        self.text = text
+        self.txt_surface = FONT.render(text, True, self.color)
+        self.active = False
+        self.message = ""
+
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(event.pos):
+                self.active = not self.active
+            else:
+                self.active = False
+            self.color = COLOR_ACTIVE if self.active else COLOR_INACTIVE
+        if event.type == pygame.KEYDOWN:
+            if self.active:
+                if event.key == pygame.K_RETURN:
+                    temp = self.text
+                    if change_assistance(temp,list_teams):
+                        self.message = "REGISTRADO"
+                    else:
+                        self.message="DESCONOCIDO"
+                    self.text = ""
+                elif event.key == pygame.K_BACKSPACE:
+                    self.text = self.text[:-1]
+                else:
+                    self.text += event.unicode
+                self.txt_surface = FONT.render(self.text, True, self.color)
+    def handle_event_end(self, event):## function for endtime
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(event.pos):
+                self.active = not self.active
+            else:
+                self.active = False
+            self.color = COLOR_ACTIVE if self.active else COLOR_INACTIVE
+        if event.type == pygame.KEYDOWN:
+            if self.active:
+                if event.key == pygame.K_RETURN:
+                    temp = self.text
+                    if change_endTime(temp,list_teams):
+                        self.message = "Listo"
+                    else:
+                        self.message="No existe"
+                    self.text = ""
+                elif event.key == pygame.K_BACKSPACE:
+                    self.text = self.text[:-1]
+                else:
+                    self.text += event.unicode
+                self.txt_surface = FONT.render(self.text, True, self.color)
+
+    def update(self):
+        # Resize the box if the text is too long.
+        width = max(200, self.txt_surface.get_width()+10)
+        self.rect.w = width
+
+    def draw(self, screen):
+        # Blit the text.
+        screen.blit(self.txt_surface, (self.rect.x+5, self.rect.y+5))
+        # Blit the rect.
+        pygame.draw.rect(screen, self.color, self.rect, 2)
+
+def main():
+    #write_alldata(list_teams)
+    clock = pygame.time.Clock()
+    #print(clock)
+    input_box_assistance = InputBox(400, 300, 800, 50)
+    input_box_exit = InputBox(400, 400, 800, 50)
+    #input_boxes = [input_box_assistance]
+    done = False
+    unsa_image = pygame.image.load("Imagenes_logos/unsa.png")
+    jaku_image = pygame.image.load("Imagenes_logos/JAKU_full_color.png")
+    unsa_image = pygame.transform.scale(unsa_image, (250, 100))
+    jaku_image = pygame.transform.scale(jaku_image, (250, 100))
+    text = "Control de Asistencia PEM"
+    entrada = "Entrada :"
+    salida = "Salida :"
+    fuente = pygame.font.Font(None, 85)
+    while not done:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+            #for box in input_boxes:
+            #    box.handle_event(event)
+            input_box_assistance.handle_event(event)
+            input_box_exit.handle_event_end(event)
+
+        #for box in input_boxes:
+        #    box.update()
+        input_box_assistance.update()
+        input_box_exit.update()
+
+        screen.fill((200, 200, 200))
+
+        screen.blit(unsa_image,(50,50))
+        screen.blit(jaku_image,(600,50))
+        #for box in input_boxes:
+        #    box.draw(screen)
+        input_box_assistance.draw(screen)
+        input_box_exit.draw(screen)
+        
+        mensaje = fuente.render(text, 2, (0, 0, 0))
+        mensaje_E = fuente.render(entrada, 2, (0, 0, 0))
+        mensaje_S = fuente.render(salida, 2, (0, 0, 0))
+        if input_box_assistance.message == "REGISTRADO":
+            verificacion = fuente.render(input_box_assistance.message,4,(0,0,255))
+        else:
+            verificacion = fuente.render(input_box_assistance.message,4,(255,0,0))
+        if input_box_exit.message == "Listo":
+            verificacion2 = fuente.render(input_box_exit.message,4,(0,255,0))
+        else:
+            verificacion2 = fuente.render(input_box_exit.message,4,(255,0,0))
+        screen.blit(verificacion2,(620,400))
+        screen.blit(verificacion, (250, 500))
+        screen.blit(mensaje, (100, 200))
+        screen.blit(mensaje_E, (100, 300))
+        screen.blit(mensaje_S, (100, 400))
+        pygame.display.flip()
+        clock.tick(60)
+        write_assitance(list_teams,file_csv)
+
+if __name__ == '__main__':
+    main()
+    workbook = Workbook()
+    worksheet = workbook.active
+    with open(file_csv, 'r') as f:
+        reader = csv.reader(f)
+        for r, row in enumerate(reader):
+            for c, col in enumerate(row):
+                for idx, val in enumerate(col.split(',')):
+                    cell = worksheet.cell(row=r+1, column=c+1)
+                    cell.value = val
+    excel = "Registro EXCEL "+strftime("%Y-%m-%d", gmtime())+".xlsx"
+    workbook.save(excel)
+    pygame.quit()
---
+++ teams.py
@@ -0,0 +1,147 @@
+import os
+import csv
+from time import gmtime, strftime
+class Team:
+    def __init__(self,name,list_dni):
+        self.name = name   
+        self.listDNI = list_dni
+        self.assistance = "NO"
+        self.date = ""
+        self.startTime = ""
+        self.endTime = ""
+    def __repr__(self):
+        return  str(self.listDNI)
+    def change_assistance(self):
+        self.assistance = "SI"
+    
+    def find_DNI(self,var):
+        try:
+          if float(var) in self.listDNI:
+            return True
+        except ValueError:
+            return 
+        return False
+    def change_endT(self):
+        hour = strftime("%H", gmtime())
+        hour = int(hour) - 5
+        self.endTime = strftime(str(hour)+":%M:%S", gmtime())
+    def change_date(self):
+        self.date = strftime("%Y-%m-%d", gmtime())
+        hour = strftime("%H", gmtime())
+        hour = int(hour) - 5
+        self.startTime = strftime(str(hour)+":%M:%S", gmtime())
+
+list_teams = []
+
+team1 = Team("academia pre  universitaria  virtual ALTO NIVEL",[41860420,41265182,41860420,72640448])
+team2 = Team("TOGO",[70326247])
+team3 = Team("t-mensajeo",[43185022,42273812])
+team4 = Team("SocialTEC",[72417249,70321701,74092883])
+team5 = Team("ALTERNATIVAS SOSTENIBLES PARA EL TRATAMIENTO DE AGUAS RESIDUALES INDUSTRIALES MEDIANTE EL USO DE HUMEDALES CONSTRUIDOS",[72463147])
+team6 = Team("PapayaHR",[74170429,74586845,74585249])
+team7 = Team("Green Car Simulator",[74170429,22995555])
+team8 = Team("YucaO2",[46498771])
+team9 = Team("AllynCose",[76738097,70552749,71498374,70553164])
+team10 = Team("Sculptor.cc",[46847148,46933133])
+team11 = Team("TURISMO VIVENCIAL",[74220497,45353939,29683967])
+team12 = Team("29683967",[72618879,71768688,70774633])
+team13 = Team("cucharas y tazas comestibles",[71635622,73707270,72966797])
+team14 = Team("AirNova",[71624415,73451618,73451618])
+team15 = Team("DR: GREEN PHAMPA",[29730413])
+team16 = Team("Grupo Geagreen",[75781967,76216312,72117672,44303131])
+team17 = Team("Gemelas MPH",[43041056,43041056])
+team18 = Team("TECNOLOGÍA E INNOVACIÓN EN LA CONSTRUCCIÓN",[47490812,74050861,77328325])
+team19 = Team("TARWIN, Antojos que Alimentan",[70178866,72427687])
+team20 = Team("VIVE TU CARRERA AHORA",[45318880])
+team21 = Team("TUTU",[71477929,73640513,75971979])
+team22 = Team("Myco-ladrillos",[71532701,76723146])
+team23 = Team("lowprovider.com",[76189211])
+team24 = Team("Safe Paths",[71581717,72205691])
+team25 = Team("Melenia",[29583636])
+team26 = Team("Aceites esenciales Olimpo",[42904635,43053791])
+team27 = Team("SCENTINET INNOVACIÓN Y PROSPECTIVA",[42172094])
+team28 = Team("Renuevate",[45632840])
+team29 = Team("GII MEDICAL TECHNOLOGIES",[76581850,47750093])
+team30 = Team("Anonidioma",[72302148,47543778])
+team31 = Team("Chocolates Caplina",[10798156])
+team32 = Team("Galletas de Chaco",[70558493,72150448,71432281,72648872])
+team33 = Team("Cápsulas de vida",[73940090,48568249,48055715])
+team34 = Team("Moda,diseño y sublimacion VALERO",[76881193])
+team35 = Team("Sticks ICE",[47042574,71305993,73112838,70512739])
+team36 = Team("JÚPITER",[77057953,71496460,71568556,76339639])
+team37 = Team("PADIS",[44474667])
+team38 = Team("Abono Green Peace",[75197207,72518750,76227413,73764719])
+team39 = Team("NUTRIALIVIO",[73872052,72539101,71459230,71438768])
+team40 = Team("IMPORTACION, VENTA Y APLICACION DE RECUBRIMIENTOS POLIMERICOS APLICABLES EN INDUSTRIA ",[47430591,46926535])
+team41 = Team("HOGAR LIMPIO JAM",[45878018,48678743,75470859,45878018])
+team42 = Team("Centro de Flotación Sensory",[43613645,74039250])
+team43 = Team("Alimentacion saludable y nutritiva del Colca para ti",[29601104,29567371])
+team44 = Team("HealthyWorld",[75693117,73193491,63193495])
+team45 = Team("C.N.E.(Creando un Nuevo Ecosistema)",[77799217,76421124,77799217,77231660])
+##team44 = Team("Inge",[40534343,2,3])
+team46 = Team("JAKU",[11,22,33,44])
+
+
+
+list_teams.append(team1)
+list_teams.append(team2)
+list_teams.append(team3)
+list_teams.append(team4)
+list_teams.append(team5)
+list_teams.append(team6)
+list_teams.append(team7)
+list_teams.append(team8)
+list_teams.append(team9)
+list_teams.append(team10)
+list_teams.append(team11)
+list_teams.append(team12)
+list_teams.append(team13)
+list_teams.append(team14)
+list_teams.append(team15)
+list_teams.append(team16)
+list_teams.append(team17)
+list_teams.append(team18)
+list_teams.append(team19)
+list_teams.append(team20)
+list_teams.append(team21)
+list_teams.append(team22)
+list_teams.append(team23)
+list_teams.append(team24)
+list_teams.append(team25)
+list_teams.append(team26)
+list_teams.append(team27)
+list_teams.append(team28)
+list_teams.append(team29)
+list_teams.append(team30)
+list_teams.append(team31)
+list_teams.append(team32)
+list_teams.append(team33)
+list_teams.append(team34)
+list_teams.append(team35)
+list_teams.append(team36)
+list_teams.append(team37)
+list_teams.append(team38)
+list_teams.append(team39)
+list_teams.append(team40)
+list_teams.append(team41)
+list_teams.append(team42)
+list_teams.append(team43)
+list_teams.append(team44)
+list_teams.append(team45)
+list_teams.append(team46)
+
+def write_alldata(list_teams):
+
+    fname = "asistencia.csv"
+    if os.path.isfile(fname):
+        return 
+    else:
+        with open('asistencia.csv', 'a', newline='') as file:
+                    fieldnames = ['Nombre del emprendimiento', 'Asistencia','Dia','Hora Entrada']
+                    writer = csv.DictWriter(file, fieldnames=fieldnames)
+                    writer.writeheader()
+                    for one_list in list_teams:
+                        writer.writerow({'Nombre del emprendimiento': one_list.name, 
+                                        'Asistencia': one_list.assistance,
+                                        'Dia':one_list.date,
+                                        'Hora Entrada':one_list.startTime})
